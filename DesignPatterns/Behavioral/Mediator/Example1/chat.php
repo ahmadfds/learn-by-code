c2923cc2a3f87a7fed42f68348c6d3341a6e26d0
Ahmad Saleh <ahmadfds@gmail.com>
2022-01-13 12:07:25 +0300

    SDLC

---
+++ DesignPatterns/Behavioral/Mediator/Example1/chat.php
@@ -0,0 +1,11 @@
+<?php
+
+class LoginForm
+{
+    public
+}
+
+class Component {}
+class TextField extends Component {}
+class PasswordField extends Component {}
+class Checkbox extends Component {}
